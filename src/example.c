bbf35ce1557345c51d39a42be35c7021c28abb79
Chinmay Garde <chinmaygarde@gmail.com>
2024-10-27 18:32:28 -0700

    Compile using the wasi toolchain.

--- src/example.c
+++ src/example.c
@@ -1,4 +1,29 @@
 
+#include <stdio.h>
+
 int main(int argc, char const* argv[]) {
+  printf("Hello from WASM with %d args.\n", argc);
   return 0;
+}
+
+void __imported_wasi_snapshot_preview1_args_get() {
+  __builtin_trap();
+}
+void __imported_wasi_snapshot_preview1_args_sizes_get() {
+  __builtin_trap();
+}
+void __imported_wasi_snapshot_preview1_fd_close() {
+  __builtin_trap();
+}
+void __imported_wasi_snapshot_preview1_fd_fdstat_get() {
+  __builtin_trap();
+}
+void __imported_wasi_snapshot_preview1_fd_seek() {
+  __builtin_trap();
+}
+void __imported_wasi_snapshot_preview1_fd_write() {
+  __builtin_trap();
+}
+void __imported_wasi_snapshot_preview1_proc_exit() {
+  __builtin_trap();
 }
--- src/unittests.cc
+++ src/unittests.cc
@@ -1,4 +1,5 @@
 #include "fixtures_location.h"
+#include "flutter/fml/mapping.h"
 #include "gtest/gtest.h"
 #include "impeller.hpp"
 #include "render_engine.h"
@@ -7,7 +8,29 @@
 namespace fiddle::testing {
 
 TEST(EmptyTest, CanCreateWasmEngine) {
+  auto wasm_blob =
+      fml::FileMapping::CreateReadOnly(TEST_BUILD_LOCATION "/example.wasm");
+  ASSERT_TRUE(wasm_blob);
   wasmtime::Engine engine;
+  wasmtime::Span<uint8_t> wasm_span(
+      const_cast<uint8_t*>(wasm_blob->GetMapping()), wasm_blob->GetSize());
+  auto module = wasmtime::Module::compile(engine, wasm_span);
+  ASSERT_TRUE(!!module);
+  wasmtime::Store store(engine);
+
+  wasmtime::Store::Context context(store);
+  wasmtime::WasiConfig wasi_config;
+  wasi_config.inherit_stderr();
+  wasi_config.inherit_stdout();
+  wasi_config.inherit_stderr();
+  auto wasi_config_result = context.set_wasi(std::move(wasi_config));
+  ASSERT_TRUE(!!wasi_config_result);
+  std::vector<wasmtime::Extern> imports;
+  auto instance = wasmtime::Instance::create(context, module.ok(), imports);
+  if (!instance) {
+    FML_LOG(IMPORTANT) << instance.err().message();
+  }
+  ASSERT_TRUE(!!instance);
 }
 
 }  // namespace fiddle::testing
